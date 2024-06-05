f52367a28db776dad03a2c2cc85c7a4d456f01e0
Jay_local <go.jhson@gmail.com>
2024-06-06 08:54:54 +0900

    Add quiz

---
+++ quiz_game/go.mod
@@ -0,0 +1,3 @@
+module github.com/gogojson/quiz_game
+
+go 1.22.2
---
+++ quiz_game/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "os"
+
+const defaultFilePath = "problems.csv"
+
+func main() {
+	// Open the CSV file
+	f, err := os.Open(defaultFilePath)
+	if err != 
+
+}
